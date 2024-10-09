e5080f808b80406fc55407dfa6e896786f139181
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-10-09 14:48:35 +0200

    Update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/button/Button";
+
+const App = ()=> {
+  return (
+  <div>
+    <Button>Click me</Button>
+    <Button>Accept</Button>
+    <Button>Cancel</Button>
+  </div>
+  );
+};
+
+export default App;
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({children}) => {
+    
+    return <button>{chindren}</button>;
+};
+
+
+export default Button;
